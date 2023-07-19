f663928162680e0130333f38c48bcdcc78f90739
solankigautam18 <gautamsolanki0218@gmail.com>
2023-07-20 03:09:47 +0530

    Freeksout webpage ready

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// var sidemenu = document.getElementById("sidemenu");
+//         function openmenu(){
+//             sidemenu.style.right = "0";
+//         }
+//         function closemenu(){
+//             sidemenu.style.right = "-250px";
+//         }
+
+// short-hand
+function toggleSidebar() {
+    var sidebar = document.querySelector('.navs ul');
+    sidebar.classList.toggle('show-sidebar');
+  }
+  
+
+       
